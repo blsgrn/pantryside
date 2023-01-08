1dc4fe994aa4cc8fc195babc987116a84c3207a9
Subramaniam Balasegaran <blsgrn@protonmail.com>
2023-01-08 06:56:21 +0530

    create Navbar.jsx

---
+++ components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div>
+      Pantryside.com, a place where you can order your favourite food online.
+      <br />
+      contact: blsgrn@tutanota.com
+    </div>
+  );
+}
+
+export default Navbar;
